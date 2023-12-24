6f6324118a1bca032e6d97f08ee1561e9ba2f7c1
RudhyDev <rudhy.pereira@gmail.com>
2023-12-24 15:18:32 -0300

    Add zod dependency

---
+++ src/domain/types.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const AddressSchema = z.object({
+  line1: z.string(),
+  loine2: z.string().optional(),
+  zipCode: z.string(),
+  city: z.string(),
+  country: z.string(),
+});
+
+export type Address = z.infer<typeof AddressSchema>;
+
